2fc3ac5a898ce36b3d789871563ecb3d7d8fb271
yousseftarekkh <yousseftarek@thewebops.com>
2020-03-02 22:15:05 +0200

    + dict added

---
+++ DCGDict.pl
@@ -0,0 +1,45 @@
+noun --> "man", "men", "boy", "boys", "box",
+         "boxes", "room", "rooms", "school",
+         "schools", "woman", "envelope", "envelopes",
+         "building", "buildings", "tree", "trees",
+         "girl", "girls", "student", "students",
+         "professor", "professors", "lecture", "lecturers",
+         "scientist", "scientists", "researcher", "researchers", "while".
+
+verb --> "worked", "pushed", "stored", "gave", "shed",
+         "liked", "climbed", "watched", "admired", "appreciated",
+         "accept", "add", "admire", "admit", "advise", "afford", "agree", "alert", "allow",
+         "amuse", "analyse", "analyze", "announce", "annoy", "answer", "apologise",
+         "appear", "applaud", "appreciate", "approve", "argue",
+         "arrange", "arrest", "arrive", "ask", "attach", "attack",
+         "attempt", "attend", "attract", "avoid".
+
+adjectives --> "young", "poor", "old", "big", "large",
+               "empty", "white", "wooden", "broken", "round",
+               "brilliant", "talented", "many", "bright", "smart", "dumb",
+               "thrilled", "sad", "attractive", "bald", "beautiful", "chubby",
+               "clean", "dazzling", "drab", "elegant", "fancy", "fit", "flabby",
+               "glamorous", "gorgeous", "handsome", "long", "magnificent",
+               "muscular", "plain", "plump".
+
+pronoun --> "they", "who", "whom", "many".
+
+conjunction --> "and", "for", "nor", "but", "or", "yet", "so".
+
+adverbs --> "secretly", "quickly", "oddly", "offensively", "officially", "often",
+            "only", "openly", "optimistically", "overconfidently", "painfully",
+            "partially", "patiently", "perfectly", "physically",
+            "playfully", "politely", "poorly", "positively", "potentially",
+            "powerfully", "promptly", "properly", "punctually", "quaintly",
+            "queasily", "queerly", "questionably", "quicker", "quickly", "quietly",
+            "quirkily", "quizzically", "randomly", "rapidly", "rarely", "readily".
+
+prepositions --> "in", "behind", "after", "for", "of", "with", "at", "from", "into",
+                 "during", "including", "until", "against", "among", "throughout",
+                 "despite", "towards", "upon", "concerning", "to", "on",
+                 "by", "about", "like".
+
+determiners --> "the", "a", "an", "this", "that",
+                "these", "those", "my", "your", "his",
+                "her", "its", "our", "their", "every",
+                "some", "many".
