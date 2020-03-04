90108fd53fe6dbc1fc936d826d827b7eda2b5d28
yousseftarekkh <yousseftarek@thewebops.com>
2020-03-04 20:16:08 +0200

    + progress

--- DCG.pl
+++ DCG.pl
@@ -1,1 +1,58 @@
-s(T, []).
+:-include('DCGDict.pl').
+
+sentence(s(NP, VP)) --> noun_phrase(NP), verb_phrase(VP).
+
+% >>> Noun Phrases >>> %
+noun_phrase(np(P)) --> pron(P).
+noun_phrase(np(P, AP)) --> pron(P), adverb_phrase(AP).
+noun_phrase(np(DP)) --> det_phrase(DP).
+noun_phrase(np(DP, PP)) --> det_phrase(DP), prep_phrase(PP).
+noun_phrase(np(DP, AP)) --> det_phrase(DP), adject_phrase(AP).
+noun_phrase(np(DP, AP, PP)) --> det_phrase(DP), adject_phrase(AP), prep_phrase(PP).
+% <<< Noun Phrases <<< %
+
+% >>> Prepositional Phrases >>> %
+noun_prep_phrase(prepp(P, NP)) --> start_prep(P), noun_phrase(NP).
+verb_prep_phrase(prepp(P, A)) --> start_prep(P), adjc(A).
+prep_phrase(PP) --> noun_prep_phrase(PP); verb_prep_phrase(PP).
+% <<< Prepositional Phrases <<< %
+
+% >>> Determiner Phrases >>> %
+det_phrase(DP) --> inf_def_det_phrase(DP); poss_det_phrase(DP); demon_det_phrase(DP); quant_det_phrase(DP). 
+inf_def_det_phrase(dp(D, N)) --> article(D), noun(N).
+inf_def_det_phrase(dp(D, AP, N)) --> article(D), adject_phrase(AP), noun(N).
+poss_det_phrase(dp(D, N)) --> poss_det(D), noun(N).
+poss_det_phrase(dp(D, AP, N)) --> poss_det(D), adject_phrase(AP), noun(N).
+demon_det_phrase(dp(D, N)) --> demon_det(D), noun(N).
+demon_det_phrase(dp(D, AP, N)) --> demon_det(D), adject_phrase(AP), noun(N).
+quant_det_phrase(dp(D, N)) --> quant_det(D), noun(N).
+quant_det_phrase(dp(D, AP, N)) --> quant_det(D), adject_phrase(AP), noun(N).
+% <<< Determiner Phrases <<< %
+
+% >>> Verb Phrases >>> %
+verb_phrase(vp(V)) --> verb(V).
+verb_phrase(vp(V1, V2)) --> verb(V1), verb_phrase(V2).
+verb_phrase(vp(V1, C, V2)) --> verb(V1), conj_phrase(C), verb_phrase(V2).
+verb_phrase(vp(V, A)) --> verb(V), adverb_phrase(A).
+verb_phrase(vp(V, NP)) --> verb(V), noun_phrase(NP).
+verb_phrase(vp(A, V, NP)) --> adv(A), verb(V), noun_phrase(NP).
+% <<< Adjectif Phrases <<< %
+
+% >>> Adjectif Phrases >>> %
+adject_phrase(ajp(A)) --> adjc(A).
+adject_phrase(ajp(A1, A2)) --> adjc(A1), adject_phrase(A2).
+adject_phrase(ajp(A1, C, A2)) --> adjc(A1), conj_phrase(C), adject_phrase(A2).
+adject_phrase(ajp(A1, A2)) --> adv(A1), adject_phrase(A2). 
+adject_phrase(ajp(V, P, NP)) --> verb(V), prep(P), noun_phrase(NP). 
+adject_phrase(ajp(A, V, P, NP)) --> adjc(A), verb(V), prep(P), noun_phrase(NP). 
+adject_phrase(ajp(P, V)) --> pron(P), verb(V). 
+adject_phrase(ajp(P, V, CP)) --> pron(P), verb(V), conj_phrase(CP).
+% <<< Adjectif Phrases <<< %
+
+% >>> Conjunctive Phrases >>> %
+conj_phrase(cp(C)) --> conj(C).
+conj_phrase(cp(C, NP)) --> conj(C), noun_phrase(NP).
+% <<< Conjunctive Phrases <<< %
+
+
+adverb_phrase(abp(A)) --> adv(A).
--- DCGDict.pl
+++ DCGDict.pl
@@ -1,45 +1,255 @@
-noun --> "man", "men", "boy", "boys", "box",
-         "boxes", "room", "rooms", "school",
-         "schools", "woman", "envelope", "envelopes",
-         "building", "buildings", "tree", "trees",
-         "girl", "girls", "student", "students",
-         "professor", "professors", "lecture", "lecturers",
-         "scientist", "scientists", "researcher", "researchers", "while".
+% ["the", "young", "boy", "who", "worked", "for", "old", "man", "pushed", "and", "stored", "a", "big", "box", "in", "large", "empty", "room", "after", "school", "woman", "gave", "poor", "whom", "they", "liked", "white", "envelope", "shed", "behind", "building", "every", "quickly", "climbed", "some", "tree", "while", "girl", "secretly", "watched", "brilliant", "students", "many", "professors", "admired", "talented", "lecturers", "appreciated", "bright", "scientists", "researchers"]
 
-verb --> "worked", "pushed", "stored", "gave", "shed",
-         "liked", "climbed", "watched", "admired", "appreciated",
-         "accept", "add", "admire", "admit", "advise", "afford", "agree", "alert", "allow",
-         "amuse", "analyse", "analyze", "announce", "annoy", "answer", "apologise",
-         "appear", "applaud", "appreciate", "approve", "argue",
-         "arrange", "arrest", "arrive", "ask", "attach", "attack",
-         "attempt", "attend", "attract", "avoid".
+% the, young, boy, who, worked, for, the, old, man, pushed, and, stored, a, big, box, in, the, large, empty, room, after, school
+noun(n(man)) --> [man].
+noun(n(men)) --> [men].
+noun(n(boy)) --> [boy].
+noun(n(boys)) --> [boys].
+noun(n(box)) --> [box].
+noun(n(boxes)) --> [boxes].
+noun(n(room)) --> [room].
+noun(n(rooms)) --> [rooms].
+noun(n(school)) --> [school].
+noun(n(schools)) --> [schools].
+noun(n(woman)) --> [woman].
+noun(n(envelope)) --> [envelope].
+noun(n(envelopes)) --> [envelopes].
+noun(n(building)) --> [building].
+noun(n(buildings)) --> [buildings].
+noun(n(tree)) --> [tree].
+noun(n(trees)) --> [trees].
+noun(n(girl)) --> [girl].
+noun(n(girls)) --> [girls].
+noun(n(student)) --> [student].
+noun(n(students)) --> [students].
+noun(n(professor)) --> [professor].
+noun(n(professors)) --> [professors].
+noun(n(lecture)) --> [lecture].
+noun(n(lecturers)) --> [lecturers].
+noun(n(scientist)) --> [scientist].
+noun(n(scientists)) --> [scientists].
+noun(n(researcher)) --> [researcher].
+noun(n(researchers)) --> [researchers].
+noun(n(while)) --> [while].
 
-adjectives --> "young", "poor", "old", "big", "large",
-               "empty", "white", "wooden", "broken", "round",
-               "brilliant", "talented", "many", "bright", "smart", "dumb",
-               "thrilled", "sad", "attractive", "bald", "beautiful", "chubby",
-               "clean", "dazzling", "drab", "elegant", "fancy", "fit", "flabby",
-               "glamorous", "gorgeous", "handsome", "long", "magnificent",
-               "muscular", "plain", "plump".
+verb(v(worked)) --> [worked].
+verb(v(pushed)) --> [pushed].
+verb(v(stored)) --> [stored].
+verb(v(gave)) --> [gave].
+verb(v(shed)) --> [shed].
+verb(v(liked)) --> [liked].
+verb(v(climbed)) --> [climbed].
+verb(v(watched)) --> [watched].
+verb(v(admired)) --> [admired].
+verb(v(appreciated)) --> [appreciated].
+verb(v(accepted)) --> [accepted].
+verb(v(added)) --> [added].
+verb(v(admired)) --> [admired].
+verb(v(admited)) --> [admited].
+verb(v(advised)) --> [advised].
+verb(v(afforded)) --> [afforded].
+verb(v(agreed)) --> [agreed].
+verb(v(alerted)) --> [alerted].
+verb(v(allowed)) --> [allowed].
+verb(v(amused)) --> [amused].
+verb(v(analysed)) --> [analysed].
+verb(v(announced)) --> [announced].
+verb(v(annoyed)) --> [annoyed].
+verb(v(answered)) --> [answered].
+verb(v(apologised)) --> [apologised].
+verb(v(appeared)) --> [appeared].
+verb(v(applauded)) --> [applauded].
+verb(v(appreciated)) --> [appreciated].
+verb(v(approved)) --> [approved].
+verb(v(argued)) --> [argued].
+verb(v(arranged)) --> [arranged].
+verb(v(arrested)) --> [arrested].
+verb(v(arrived)) --> [arrived].
+verb(v(asked)) --> [asked].
+verb(v(attached)) --> [attached].
+verb(v(attacked)) --> [attacked].
+verb(v(attempted)) --> [attempted].
+verb(v(attended)) --> [attended].
+verb(v(attracted)) --> [attracted].
+verb(v(avoided)) --> [avoided].
+verb(v(worked)) --> [worked].
 
-pronoun --> "they", "who", "whom", "many".
 
-conjunction --> "and", "for", "nor", "but", "or", "yet", "so".
+adjc(aj(young)) --> [young].
+adjc(aj(poor)) --> [poor].
+adjc(aj(old)) --> [old].
+adjc(aj(big)) --> [big].
+adjc(aj(large)) --> [large].
+adjc(aj(empty)) --> [empty].
+adjc(aj(white)) --> [white].
+adjc(aj(wooden)) --> [wooden].
+adjc(aj(broken)) --> [broken].
+adjc(aj(round)) --> [round].
+adjc(aj(brilliant)) --> [brilliant].
+adjc(aj(talented)) --> [talented].
+adjc(aj(many)) --> [many].
+adjc(aj(bright)) --> [bright].
+adjc(aj(smart)) --> [smart].
+adjc(aj(dumb)) --> [dumb].
+adjc(aj(thrilled)) --> [thrilled].
+adjc(aj(sad)) --> [sad].
+adjc(aj(attractive)) --> [attractive].
+adjc(aj(bald)) --> [bald].
+adjc(aj(beautiful)) --> [beautiful].
+adjc(aj(chubby)) --> [chubby].
+adjc(aj(clean)) --> [clean].
+adjc(aj(dazzling)) --> [dazzling].
+adjc(aj(drab)) --> [drab].
+adjc(aj(elegant)) --> [elegant].
+adjc(aj(fancy)) --> [fancy].
+adjc(aj(fit)) --> [fit].
+adjc(aj(flabby)) --> [flabby].
+adjc(aj(glamorous)) --> [glamorous].
+adjc(aj(gorgeous)) --> [gorgeous].
+adjc(aj(handsome)) --> [handsome].
+adjc(aj(long)) --> [long].
+adjc(aj(magnificent)) --> [magnificent].
+adjc(aj(muscular)) --> [muscular].
+adjc(aj(plain)) --> [plain].
+adjc(aj(plump)) --> [plump].
 
-adverbs --> "secretly", "quickly", "oddly", "offensively", "officially", "often",
-            "only", "openly", "optimistically", "overconfidently", "painfully",
-            "partially", "patiently", "perfectly", "physically",
-            "playfully", "politely", "poorly", "positively", "potentially",
-            "powerfully", "promptly", "properly", "punctually", "quaintly",
-            "queasily", "queerly", "questionably", "quicker", "quickly", "quietly",
-            "quirkily", "quizzically", "randomly", "rapidly", "rarely", "readily".
+pron(pr(they)) --> [they].
+pron(pr(he)) --> [he].
+pron(pr(she)) --> [she].
+pron(pr(it)) --> [it].
+pron(pr(who)) --> [who].
+pron(pr(whom)) --> [whom].
+pron(pr(many)) --> [many].
 
-prepositions --> "in", "behind", "after", "for", "of", "with", "at", "from", "into",
-                 "during", "including", "until", "against", "among", "throughout",
-                 "despite", "towards", "upon", "concerning", "to", "on",
-                 "by", "about", "like".
+conj(c(and)) --> [and].
+conj(c(for)) --> [for].
+conj(c(nor)) --> [nor].
+conj(c(but)) --> [but].
+conj(c(or)) --> [or].
+conj(c(yet)) --> [yet].
+conj(c(so)) --> [so].
 
-determiners --> "the", "a", "an", "this", "that",
-                "these", "those", "my", "your", "his",
-                "her", "its", "our", "their", "every",
-                "some", "many".
+adv(ab(secretly)) --> [secretly].
+adv(ab(quickly)) --> [quickly].
+adv(ab(oddly)) --> [oddly].
+adv(ab(offensively)) --> [offensively].
+adv(ab(officially)) --> [officially].
+adv(ab(often)) --> [often].
+adv(ab(only)) --> [only].
+adv(ab(openly)) --> [openly].
+adv(ab(optimistically)) --> [optimistically].
+adv(ab(overconfidently)) --> [overconfidently].
+adv(ab(painfully)) --> [painfully].
+adv(ab(partially)) --> [partially].
+adv(ab(patiently)) --> [patiently].
+adv(ab(perfectly)) --> [perfectly].
+adv(ab(physically)) --> [physically].
+adv(ab(playfully)) --> [playfully].
+adv(ab(politely)) --> [politely].
+adv(ab(poorly)) --> [poorly].
+adv(ab(positively)) --> [positively].
+adv(ab(potentially)) --> [potentially].
+adv(ab(powerfully)) --> [powerfully].
+adv(ab(promptly)) --> [promptly].
+adv(ab(properly)) --> [properly].
+adv(ab(punctually)) --> [punctually].
+adv(ab(quaintly)) --> [quaintly].
+adv(ab(queasily)) --> [queasily].
+adv(ab(queerly)) --> [queerly].
+adv(ab(questionably)) --> [questionably].
+adv(ab(quicker)) --> [quicker].
+adv(ab(quickly)) --> [quickly].
+adv(ab(quietly)) --> [quietly].
+adv(ab(quirkily)) --> [quirkily].
+adv(ab(quizzically)) --> [quizzically].
+adv(ab(randomly)) --> [randomly].
+adv(ab(rapidly)) --> [rapidly].
+adv(ab(rarely)) --> [rarely].
+adv(ab(readily)) --> [readily].
+adv(ab(gently)) --> [gently].
+
+
+% # Prepositions # %
+
+% ## Start Preposition ## %
+start_prep(sp(to)) --> [to].
+start_prep(sp(of)) --> [of].
+start_prep(sp(about)) --> [about].
+start_prep(sp(at)) --> [at].
+start_prep(sp(before)) --> [before].
+start_prep(sp(after)) --> [after].
+start_prep(sp(by)) --> [by].
+start_prep(sp(behind)) --> [behind].
+start_prep(sp(during)) --> [during].
+start_prep(sp(for)) --> [for].
+start_prep(sp(from)) --> [from].
+start_prep(sp(in)) --> [in].
+start_prep(sp(over)) --> [over].
+start_prep(sp(under)) --> [under].
+start_prep(sp(with)) --> [with].
+
+% ## Careless Preposition ## %
+c_prep(prp(into)) --> [into].
+c_prep(prp(including)) --> [including].
+c_prep(prp(until)) --> [until].
+c_prep(prp(against)) --> [against].
+c_prep(prp(among)) --> [among].
+c_prep(prp(throughout)) --> [throughout].
+c_prep(prp(despite)) --> [despite].
+c_prep(prp(towards)) --> [towards].
+c_prep(prp(upon)) --> [upon].
+c_prep(prp(concerning)) --> [concerning].
+c_prep(prp(on)) --> [on].
+c_prep(prp(like)) --> [like].
+
+prep(P) --> start_prep(P); c_prep(P).
+
+
+% # Determiners # %
+
+% ## Possessive Determiners ## %
+poss_det(d(my)) --> [my].
+poss_det(d(your)) --> [your].
+poss_det(d(their)) --> [their].
+poss_det(d(our)) --> [our].
+poss_det(d(his)) --> [his].
+poss_det(d(her)) --> [her].
+poss_det(d(its)) --> [its].
+poss_det(d(her)) --> [her].
+
+% ## Demonstrative Determiners ## %
+demon_det(d(this)) --> [this].
+demon_det(d(that)) --> [that].
+demon_det(d(these)) --> [these].
+demon_det(d(those)) --> [those].
+
+% ## Articles ## %
+article(a(a)) --> [a].
+article(a(an)) --> [an].
+article(a(the)) --> [the].
+
+% ## Quantifiers ## %
+quant_det(qd(every)) --> [every].
+quant_det(qd(many)) --> [many].
+quant_det(qd(no)) --> [no].
+quant_det(qd(some)) --> [some].
+
+det(D) --> poss_det(D); demon_det(D); article(D); quant_det(D).
+% det(d(every)) --> [every].
+% det(d(some)) --> [some].
+% det(d(many)) --> [many].
+% det(d(the)) --> [the].
+% det(d(a)) --> [a].
+% det(d(an)) --> [an].
+% det(d(this)) --> [this].
+% det(d(that)) --> [that].
+% det(d(these)) --> [these].
+% det(d(those)) --> [those].
+% det(d(my)) --> [my].
+% det(d(your)) --> [your].
+% det(d(his)) --> [his].
+% det(d(her)) --> [her].
+% det(d(its)) --> [its].
+% det(d(our)) --> [our].
+% det(d(their)) --> [their].
+
